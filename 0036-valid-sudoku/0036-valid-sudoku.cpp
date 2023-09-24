ebccf674d63b952710edb9639db6a20ff019230c
Naitik Kapadia <88614335+KapadiaNaitik@users.noreply.github.com>
2023-09-24 22:35:03 +0530

    Time: 27 ms (14.97%), Space: 19.8 MB (43.72%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -32,75 +32,96 @@ public:
             }
           
         }
-        for(int i=0;i<board.size();i=i+3){
-            set<char> temp;
-            cout<<"Iteration"<<i<<endl;
-            for(int j=0;j<3;j++){
-                
-                for(int k=0;k<3;k++){
-                    cout<<k<<" "<<j+i<<endl;
-                    len=temp.size();
-
-                    if(isdigit(board[k][j+i])){
-
-                    temp.insert(board[k][j+i]);
-                    if(len==temp.size()){
-                        return false;
-                    }
-                    }
-                    
+        for (int i = 0; i < 9; i++) {
+    set<char> temp;
+    cout << "Iteration" << i << endl;
+    int row_start = (i / 3) * 3; 
+    int col_start = (i % 3) * 3; 
+    
+    for (int j = 0; j < 3; j++) {
+        for (int k = 0; k < 3; k++) {
+            cout << col_start + k << " " << row_start + j << endl;
+            len = temp.size();
+            
+            if (isdigit(board[col_start + k][row_start + j])) {
+                temp.insert(board[col_start + k][row_start + j]);
+                if (len == temp.size()) {
+                    return false;
                 }
-
-
-
             }
         }
-          for(int i=0;i<board.size();i=i+3){
-            set<char> temp;
-            cout<<"Iteration"<<i<<endl;
-            for(int j=0;j<3;j++){
+    }
+}
+
+        // for(int i=0;i<board.size();i=i+3){
+        //     set<char> temp;
+        //     cout<<"Iteration"<<i<<endl;
+        //     for(int j=0;j<3;j++){
                 
-                for(int k=3;k<6;k++){
-                    cout<<k<<" "<<j+i<<endl;
-                    len=temp.size();
+        //         for(int k=0;k<3;k++){
+        //             cout<<k<<" "<<j+i<<endl;
+        //             len=temp.size();
 
-                    if(isdigit(board[k][j+i])){
+        //             if(isdigit(board[k][j+i])){
 
-                    temp.insert(board[k][j+i]);
-                    if(len==temp.size()){
-                        return false;
-                    }
-                    }
+        //             temp.insert(board[k][j+i]);
+        //             if(len==temp.size()){
+        //                 return false;
+        //             }
+        //             }
                     
-                }
+        //         }
 
 
 
-            }
-        }
-         for(int i=0;i<board.size();i=i+3){
-            set<char> temp;
-            cout<<"Iteration"<<i<<endl;
-            for(int j=0;j<3;j++){
+        //     }
+        // }
+        //   for(int i=0;i<board.size();i=i+3){
+        //     set<char> temp;
+        //     cout<<"Iteration"<<i<<endl;
+        //     for(int j=0;j<3;j++){
                 
-                for(int k=6;k<9;k++){
-                    cout<<k<<" "<<j+i<<endl;
-                    len=temp.size();
+        //         for(int k=3;k<6;k++){
+        //             cout<<k<<" "<<j+i<<endl;
+        //             len=temp.size();
 
-                    if(isdigit(board[k][j+i])){
+        //             if(isdigit(board[k][j+i])){
 
-                    temp.insert(board[k][j+i]);
-                    if(len==temp.size()){
-                        return false;
-                    }
-                    }
+        //             temp.insert(board[k][j+i]);
+        //             if(len==temp.size()){
+        //                 return false;
+        //             }
+        //             }
                     
-                }
+        //         }
 
 
 
-            }
-        }
+        //     }
+        // }
+        //  for(int i=0;i<board.size();i=i+3){
+        //     set<char> temp;
+        //     cout<<"Iteration"<<i<<endl;
+        //     for(int j=0;j<3;j++){
+                
+        //         for(int k=6;k<9;k++){
+        //             cout<<k<<" "<<j+i<<endl;
+        //             len=temp.size();
+
+        //             if(isdigit(board[k][j+i])){
+
+        //             temp.insert(board[k][j+i]);
+        //             if(len==temp.size()){
+        //                 return false;
+        //             }
+        //             }
+                    
+        //         }
+
+
+
+        //     }
+        // }
         //   for(int i=0;i<board.size();i=i+3){
         //     set<char> temp;
         //     for(int j=3;j<6;j++){
